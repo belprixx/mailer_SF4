ac2d35c806fe9f1ba4a2c9e07f93e545172f2eca
Theo Tison <tison_t@etna-alternance.net>
2018-09-21 15:22:56 +0200

    up

--- mailerClo5/src/Controller/MailController.php
+++ mailerClo5/src/Controller/MailController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MailController extends Controller
@@ -22,13 +23,22 @@ class MailController extends Controller
     /**
      * @Route("/mail", name="send_mail", methods={"POST"})
      */
-    public function sendMail()
+    public function sendMail(Request $request, \Swift_Mailer $mailer)
     {
-//        return $this->json([
-//            'message' => 'Welcome to your new controller!',
-//            'path' => 'src/Controller/MailController.php',
-//        ]);
-        return new JsonResponse("test", 200);
+        $recipent = $request->request->get("recipent");
+        $content = $request->request->get("content");
+        $subject = $request->request->get("subject");
+        $message = (new \Swift_Message($subject))
+                    ->setFrom($this->container->getParameter('fromMail'))
+                    ->setTo($recipent)
+                    ->setBody(
+                        $this->renderView(
+                            "emails/$content.html.twig"
+                        ),
+                        'text/html'
+                    );
+        $mailer->send($message);
+        return new JsonResponse("E-mail send", 200);
     }
 
 }
